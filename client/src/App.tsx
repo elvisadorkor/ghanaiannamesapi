20147caa90da7a9c6968e6d52dadd1490d3a8c9a
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-05-25 16:41:32 +0200

    Add header with Ghanaian flag svg and apply some styles.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,15 @@
 import "./index.css";
+import ghanaFlag from "../public/flag_of_ghana.svg";
 
 function App() {
   return (
     <div className="App">
-      <h2>Ghanaian Day Names</h2>
+      <header className="header">
+        <img src={ghanaFlag} alt="Ghanaian flag" className="ghana-flag" />
+
+        <h1>Ghanaian Day Names</h1>
+      </header>
+      <section>More components to follow</section>
     </div>
   );
 }
