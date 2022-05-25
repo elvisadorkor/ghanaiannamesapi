a9156499b1c1e3aaf705f6111b87c4d8678b2b07
Jeffery Adorkor <adorkor.jeffery@gmail.com>
2022-05-25 23:54:23 +0200

    Move backend files into server folder.

---
+++ server/Models/GhanaianNames.cs
@@ -0,0 +1,11 @@
+namespace ghanaiannamesapi.server.Models;
+
+public class Tribe
+{
+    public int id { get; set; }
+    public string Name { get; }
+    public string Region { get; }
+
+    public Dictionary<string, string[]> MaleDayNames { get; set; }
+    public Dictionary<string, string[]> FemaleDayNames { get; set; }
+}
